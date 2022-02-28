d0104a069a5c287ffc98589e8e7dba660f78df13
fvn70 <fvn_reg@mail.ru>
2022-02-28 16:04:50 +0700

    Stage 2/4: Text preprocessing pipeline

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,9 +1,12 @@
+import string
 import nltk as nltk
 from lxml import etree
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
-def token_text(text):
-    tokens = nltk.tokenize.word_tokenize(text.lower())
-    dic = {t: tokens.count(t) for t in tokens}
+
+def sort_text(text):
+    dic = {t: text.count(t) for t in text}
     dic = {k: dic[k] for k in sorted(dic, key=dic.get, reverse=True)}
     cnts = sorted({dic[k] for k in dic}, reverse=True)
     lst = []
@@ -14,14 +17,30 @@ def token_text(text):
     print(' '.join(lst[:5]), '\n')
 
 
+def lemma_text(text):
+    tizer = WordNetLemmatizer()
+    stw = stopwords.words('english')
+    pnkt = list(string.punctuation)
+    lemmas = []
+    for word in text:
+        lemma = tizer.lemmatize(word)
+        if not lemma in stw + pnkt:
+            lemmas.append(lemma)
+    return lemmas
+
+
 def main():
+    # nltk.download('wordnet')
+    # nltk.download('stopwords')
+    # nltk.download('omw-1.4')
     corpus = etree.parse('news.xml').getroot()
     news = corpus[0]
     for n in news:
         head = n[0].text
         print(f'{head}:')
         text = n[1].text
-        token_text(text)
+        tokens = nltk.tokenize.word_tokenize(text.lower())
+        sort_text(lemma_text(tokens))
 
 
 if __name__ == '__main__':
